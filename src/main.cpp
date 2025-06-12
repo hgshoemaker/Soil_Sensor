cc584394882fde0f8f765653c05a93d036b59211
H.G. Shoemaker <hgshoemaker@proton.me>
2025-06-12 05:29:09 -0500

    Working now

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,5 @@
 #include <Arduino.h>
 #include <NimBLEDevice.h>
-//#include <NimBLEServer.h>
-//#include <NimBLEUtils.h>
-//#include <NimBLE2904.h>
 
 #define RS485_DE 21
 #define RS485_RE 22
@@ -20,63 +17,95 @@ const byte sali[] = {0x01, 0x03, 0x00, 0x07, 0x00, 0x01, 0xe3, 0x5b};
 const byte tds[]  = {0x01, 0x03, 0x00, 0x08, 0x00, 0x01, 0xe0, 0x58};
 byte values[11];
 
+
 // BLE UUIDs for Nordic UART Service
 #define SERVICE_UUID        "6E400001-B5A3-F393-E0A9-E50E24DCCA9E"
 #define CHARACTERISTIC_UUID_TX "6E400003-B5A3-F393-E0A9-E50E24DCCA9E"
+#define CHARACTERISTIC_UUID_RX "6E400002-B5A3-F393-E0A9-E50E24DCCA9E"
 
-NimBLECharacteristic *pTxCharacteristic;
-bool deviceConnected = false;
+NimBLECharacteristic* pTxCharacteristic;
+NimBLECharacteristic* pRxCharacteristic;
 
-// Example: Send a notification with dummy data if a device is connected
-void sendNimBLEData(const String& data) {
-  if (deviceConnected && pTxCharacteristic) {
-    pTxCharacteristic->setValue(data.c_str());
+// Global sums for sensor averages
+float humiditySum = 0, temperatureSum = 0;
+float conductivitySum = 0, tdsSum = 0, salinitySum = 0;
+float phSum = 0, nitroSum = 0, phosSum = 0, potaSum = 0;
+
+class ServerCallbacks : public NimBLEServerCallbacks {
+    void onConnect(NimBLEServer* pServer, NimBLEConnInfo& connInfo) override {
+        Serial.println("iPhone connected!");
+    }
+    void onDisconnect(NimBLEServer* pServer, NimBLEConnInfo& connInfo, int reason) override {
+        Serial.println("iPhone disconnected!");
+        NimBLEDevice::getAdvertising()->start(); // Restart advertising
+        Serial.println("Advertising restarted, waiting for connection...");
+    }
+};
+
+// Add this function to send BLE data
+void sendBleData(float humiditySum, float temperatureSum, float conductivitySum, float tdsSum, float salinitySum, float phSum, float nitroSum, float phosSum, float potaSum) {
+    String bleMsg;
+    bleMsg += "Humidity (avg) = " + String(humiditySum / 10.0, 1) + " %\n";
+    bleMsg += "Temperature (avg) = " + String(temperatureSum / 10.0, 1) + " deg.C\n";
+    bleMsg += "Conductivity (avg) = " + String(conductivitySum / 10.0, 1) + " uS/cm\n";
+    bleMsg += "TDS (avg) = " + String(tdsSum / 10.0, 1) + " mg/L\n";
+    bleMsg += "Salinity (avg) = " + String(salinitySum / 10.0, 1) + " ppm\n";
+    bleMsg += "pH (avg) = " + String(phSum / 10.0, 1) + "\n";
+    bleMsg += "Nitrogen (avg) = " + String(nitroSum / 10.0, 1) + " mg/L\n";
+    bleMsg += "Phosphorus (avg) = " + String(phosSum / 10.0, 1) + " mg/L\n";
+    bleMsg += "Potassium (avg) = " + String(potaSum / 10.0, 1) + " mg/L\n\n\n\n\n\n\n\n";
+
+    pTxCharacteristic->setValue(bleMsg.c_str());
     pTxCharacteristic->notify();
-  }
+    Serial.println("Sent to iPhone:\n" + bleMsg);
 }
 
-class MyServerCallbacks: public NimBLEServerCallbacks {
-    void onConnect(NimBLEServer* pServer) {
-        Serial.println("[BLE] client connected.");
-        deviceConnected = true;
-    }
-    void onDisconnect(NimBLEServer* pServer) {
-        Serial.println("[BLE] client disconnected");
-        deviceConnected = false;
-        NimBLEDevice::startAdvertising(); // Restart advertising after disconnect
-        Serial.println("[BLE] advertising restarted");
+class RxCallbacks : public NimBLECharacteristicCallbacks {
+    void onWrite(NimBLECharacteristic* pCharacteristic, NimBLEConnInfo& connInfo) override {
+        std::string value = pCharacteristic->getValue();
+        Serial.print("Received from iPhone: ");
+        Serial.println(value.c_str());
+        // Call sendBleData with the latest averages
+        extern float humiditySum, temperatureSum, conductivitySum, tdsSum, salinitySum, phSum, nitroSum, phosSum, potaSum;
+        sendBleData(humiditySum, temperatureSum, conductivitySum, tdsSum, salinitySum, phSum, nitroSum, phosSum, potaSum);
     }
 };
 
 void setup() {
-  Serial.begin(115200);
-  Serial.println("ESP32 Soil Sensor starting...");
-  Serial1.begin(9600, SERIAL_8N1, RXD2, TXD2);
-  pinMode(RS485_DE, OUTPUT);
-  pinMode(RS485_RE, OUTPUT);
-  digitalWrite(RS485_DE, LOW); // Receive mode
-  digitalWrite(RS485_RE, LOW); // Receive mode
-  delay(1000);
+    Serial.begin(115200);
+    Serial.println("ESP32 Soil Sensor starting...");
+    Serial1.begin(9600, SERIAL_8N1, RXD2, TXD2);
+    pinMode(RS485_DE, OUTPUT);
+    pinMode(RS485_RE, OUTPUT);
+    digitalWrite(RS485_DE, LOW); // Receive mode
+    digitalWrite(RS485_RE, LOW); // Receive mode
+    delay(1000);
 
-  // NimBLE UART setup
-  NimBLEDevice::init("ESP32-SoilSensor");
-  NimBLEServer *pServer = NimBLEDevice::createServer();
-  pServer->setCallbacks(new MyServerCallbacks());
-  NimBLEService *pService = pServer->createService(SERVICE_UUID);
-  pTxCharacteristic = pService->createCharacteristic(
-    CHARACTERISTIC_UUID_TX,
-    NIMBLE_PROPERTY::NOTIFY
-  );
-  //pTxCharacteristic->addDescriptor(new NimBLE2904());
-  pService->start();
-  NimBLEAdvertising *pAdvertising = NimBLEDevice::getAdvertising();
-  pAdvertising->addServiceUUID(SERVICE_UUID);
-  // pAdvertising->setScanResponse(true); // Not available in NimBLEAdvertising
-  //NimBLEDevice::startAdvertising();
-  pAdvertising->start();
-  Serial.println("NimBLE UART started, advertising...");
+    NimBLEDevice::init("ESP32-SoilSensor");
+    NimBLEServer* pServer = NimBLEDevice::createServer();
+    pServer->setCallbacks(new ServerCallbacks());
+
+    NimBLEService* pService = pServer->createService(SERVICE_UUID);
+
+    pTxCharacteristic = pService->createCharacteristic(
+        CHARACTERISTIC_UUID_TX,
+        NIMBLE_PROPERTY::NOTIFY | NIMBLE_PROPERTY::READ
+    );
+
+    pRxCharacteristic = pService->createCharacteristic(
+        CHARACTERISTIC_UUID_RX,
+        NIMBLE_PROPERTY::WRITE | NIMBLE_PROPERTY::WRITE_NR
+    );
+    pRxCharacteristic->setCallbacks(new RxCallbacks());
+
+    pService->start();
+
+    NimBLEAdvertising* pAdvertising = NimBLEDevice::getAdvertising();
+    pAdvertising->addServiceUUID(SERVICE_UUID);
+    pAdvertising->start();
+
+    Serial.println("Waiting for iPhone to connect...");
 }
-
 int readModbusValue(const byte* cmd, int scale = 1, int offset = 3) {
   while (Serial1.available()) Serial1.read();
   digitalWrite(RS485_DE, HIGH);
@@ -175,16 +204,27 @@ void readHumidityAndTemperature() {
 }
 
 void loop() {
-  /*
+  // Reset the global variables:
+  humiditySum = temperatureSum = 0;
+  conductivitySum = tdsSum = salinitySum = 0;
+  phSum = nitroSum = phosSum = potaSum = 0;
+
   // Arrays to hold 10 readings for each parameter */
-  float humidityArr[10], temperatureArr[10]; /*
+  float humidityArr[10], temperatureArr[10];
   float conductivityArr[10], tdsArr[10], salinityArr[10];
   float phArr[10], nitroArr[10], phosArr[10], potaArr[10];
-*/
+
+  // Sums for averaging
+  // float humiditySum = 0, temperatureSum = 0;
+  // float conductivitySum = 0, tdsSum = 0, salinitySum = 0;
+  // float phSum = 0, nitroSum = 0, phosSum = 0, potaSum = 0;
+  
   // Take 10 readings
-  float humidity = 0.0, temperature = 0.0;
   for (int i = 0; i < 10; i++) {
     // --- Humidity & Temperature ---
+    float humidity = 0;
+    float temperature = 0;
+    float conductivity = 0;
     {
       // Clear Serial1 buffer
       while (Serial1.available()) Serial1.read();
@@ -215,7 +255,7 @@ void loop() {
     temperatureArr[i] = temperature;
   
     
-/*
+
     // --- Conductivity ---
     {
       while (Serial1.available()) Serial1.read();
@@ -347,13 +387,10 @@ void loop() {
   }
   
   // Calculate averages
-  */float humiditySum = 0, temperatureSum = 0; /*
-  float conductivitySum = 0, tdsSum = 0, salinitySum = 0;
-  float phSum = 0, nitroSum = 0, phosSum = 0, potaSum = 0;
-  */for (int i = 0; i < 10; i++) {
+  for (int i = 0; i < 10; i++) {
     humiditySum += humidityArr[i];
     temperatureSum += temperatureArr[i];
-    /*conductivitySum += conductivityArr[i];
+    conductivitySum += conductivityArr[i];
     tdsSum += tdsArr[i];
     salinitySum += salinityArr[i];
     phSum += phArr[i];
@@ -361,7 +398,7 @@ void loop() {
     phosSum += phosArr[i];
     potaSum += potaArr[i];
   }
-*/
+
   Serial.print("Humidity (avg) = ");
   Serial.print(humiditySum / 10.0, 1);
   Serial.println(" %");
@@ -369,7 +406,7 @@ void loop() {
   Serial.print("Temperature (avg) = ");
   Serial.print(temperatureSum / 10.0, 1);
   Serial.println(" deg.C");
-/*
+
   Serial.print("Conductivity (avg) = ");
   Serial.print(conductivitySum / 10.0, 1);
   Serial.println(" uS/cm");
@@ -398,13 +435,22 @@ void loop() {
   Serial.println(" mg/L");
 
   Serial.println();
-  delay(2000); // Wait before next set of averages
-  */
+  delay(10000); // Wait before next set of averages
 
-  // Debug print to confirm loop is running
-  Serial.println("Loop running...");
-  delay(1000);
+  // --- BLE Notify to iPhone ---
+  if (NimBLEDevice::getServer()->getConnectedCount() > 0) {
+      String bleMsg = "Humidity (avg) = " + String(humiditySum / 10.0, 1) + " %\n";
+      bleMsg += "Temperature (avg) = " + String(temperatureSum / 10.0, 1) + " deg.C\n";
+      bleMsg += "Conductivity (avg) = " + String(conductivitySum / 10.0, 1) + " uS/cm\n";
+      bleMsg += "TDS (avg) = " + String(tdsSum / 10.0, 1) + " mg/L\n";
+      bleMsg += "Salinity (avg) = " + String(salinitySum / 10.0, 1) + " ppm\n";
+      bleMsg += "pH (avg) = " + String(phSum / 10.0, 1) + "\n";
+      bleMsg += "Nitrogen (avg) = " + String(nitroSum / 10.0, 1) + " mg/L\n";
+      bleMsg += "Phosphorus (avg) = " + String(phosSum / 10.0, 1) + " mg/L\n";
+      bleMsg += "Potassium (avg) = " + String(potaSum / 10.0, 1) + " mg/L\n\n\n\n\n\n\n\n\n";
+
+      pTxCharacteristic->setValue(bleMsg.c_str());
+      pTxCharacteristic->notify();
+      Serial.println("Sent to iPhone:\n" + bleMsg);
+  }
 }
-// This closing brace was missing in the original code snippet
-} // End of loop function
-// Add this closing brace to properly terminate the loop() function
